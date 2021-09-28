faefb1f98a4a8d7ef39b014f9d0b48f68ee80c5f
Romain Bourel <romainbourel@outlook.fr>
2021-09-28 19:26:45 +0200

    create list of game in reverse date

--- Ludo-take/src/Controller/GameController.php
+++ Ludo-take/src/Controller/GameController.php
@@ -14,12 +14,23 @@ class GameController extends AbstractController
 {
     /**
      * Methode who show a game by this slug
-     * @Route("/{slug}", name="slug")
+     * @Route("/{slug}", name="slug", priority=-1)
      */
     public function index(string $slug, GameRepository $gameRepository): Response
     {
         return $this->render('game/slug.html.twig', [
             'game' => $gameRepository->findBy(['slug' => $slug])[0],
+        ]);
+    }
+
+    /**
+     * Methode who show all the game in the order of most recent to older
+     * @Route("/liste", name="list")
+     */
+    public function list(GameRepository $gameRepository): Response
+    {
+        return $this->render('game/slug.html.twig', [
+            'game' => $gameRepository->findBy([], ['created_at' => 'DESC']),
         ]);
     }
 }
