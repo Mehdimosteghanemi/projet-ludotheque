85011f48f31de70f7bfa1f0e730fc6941333766e
Romain Bourel <romainbourel@outlook.fr>
2021-09-28 22:03:30 +0200

    use the game in bdd in home page make loop for this

--- Ludo-take/src/Controller/HomeController.php
+++ Ludo-take/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,10 +15,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(GameRepository $gameRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'games' => $gameRepository->findBy([], ['created_at' => 'DESC']),
         ]);
     }
 }
