7aff969fa3d7a04c0db1566c9360f6b1f25e924a
mehdi mosteghanemi <mehdi.mosteghanemi@gmail.com>
2021-09-28 14:59:19 +0200

    creation of the findAll method to display the entire list of games

--- Ludo-take/src/Controller/GameController.php
+++ Ludo-take/src/Controller/GameController.php
@@ -2,19 +2,34 @@
 
 namespace App\Controller;
 
+use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/jeux", name="game_")
+ * 
+ * @IsGranted("ROLE_USER")
+ */
 class GameController extends AbstractController
 {
+
     /**
-     * @Route("/game", name="game")
+     * Method used to display the whole list
+     * 
+     * URL : /jeux/  NOM : game_index
+     * 
+     * @Route("/", name="index")
+     * 
      */
-    public function index(): Response
+    public function index(GameRepository $gameRepository): Response
     {
+
+        $gamesList = $gameRepository->findAll();
+
         return $this->render('game/index.html.twig', [
-            'controller_name' => 'GameController',
+            'gamesList' => $gamesList
         ]);
     }
 }
