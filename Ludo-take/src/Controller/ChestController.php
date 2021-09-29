6615997b1acb02c689c8bbba99520a16f996e9be
Romain Bourel <romainbourel@outlook.fr>
2021-09-29 14:08:31 +0200

    show in the chest the produse in order for the user

--- Ludo-take/src/Controller/ChestController.php
+++ Ludo-take/src/Controller/ChestController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Repository\OrderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 /**
  * @Route("/coffre", name="chest_")
@@ -14,10 +16,11 @@ class ChestController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(Security $security): Response
     {
         return $this->render('chest/index.html.twig', [
             'controller_name' => 'ChestController',
+            'user' => $security->getUser(),
         ]);
     }
 }
