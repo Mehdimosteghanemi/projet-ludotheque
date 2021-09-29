d2db8887c7dcdba382d72af8b79759490e244314
Sinan <bolataslansinan@gmail.com>
2021-09-29 15:21:34 +0200

    404 not found

---
+++ Ludo-take/src/Controller/NotFoundController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/", name="home_")
+ */
+
+class NotFoundController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('not_found/index.html.twig', [
+            'controller_name' => 'NotFoundController',
+        ]);
+    }
+}
