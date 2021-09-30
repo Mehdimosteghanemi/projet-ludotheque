632ea23f0b30c5cd6f67da9ceba42e39519e8947
Sinan <bolataslansinan@gmail.com>
2021-09-30 15:07:01 +0200

    Not Found Create

---
+++ Ludo-take/src/Controller/NotFoundController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NotFoundController extends AbstractController
+{
+    /**
+     * @Route("/{string<.*>}", name="not_found", priority="-10")
+     */
+    public function index(): Response
+    {
+        return $this->render('not_found/index.html.twig', [
+            'controller_name' => 'NotFoundController',
+        ]);
+    }
+}
