eddfbf7ee053951b058c59603c8f08d3c3dd11fc
Romain Bourel <romainbourel@outlook.fr>
2021-09-30 22:32:52 +0200

    create the command form, the controller who work whith, update the template to show order

--- Ludo-take/src/Controller/ChestController.php
+++ Ludo-take/src/Controller/ChestController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\OrderRepository;
+use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,7 +15,7 @@ use Symfony\Component\Security\Core\Security;
 class ChestController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="index", methods={"GET"})
      */
     public function index(Security $security): Response
     {
@@ -22,5 +23,53 @@ class ChestController extends AbstractController
             'controller_name' => 'ChestController',
             'user' => $security->getUser(),
         ]);
+    }
+
+    /**
+     * Method to update the status command.
+     * Use the form post with checkbox.
+     * @Route("/", name="update", methods={"POST"})
+     */
+    public function update(Security $security, OrderRepository $orderRepository): Response
+    {
+        // variable who check if there are changement or not
+        $oneWasChecked = false;
+
+        $user = $security->getUser();
+        // loop in every data on superglobal post (the form send the game id in key and if it was selected(checked) or not in value)
+        foreach ($_POST as $gameId => $checked) {
+            // if it was checked we search the order whith the id game and user, we used almost the status argument if the user have already take the same game in the past 
+            if ($checked) {
+                // findBy give the result in array (we know they cant have two game whith status 1 for the same user we take the first and unique key with [0]])
+                $order = $orderRepository->findBy(['games' => $gameId, 'users' => $user, "status" => 1])[0];
+                // checked if is not null
+                if ($order){
+                    // pass the status to 2 (status for send) and create the command date
+                    $order->setStatus(2);
+                    $order->setDateCommand(new DateTimeImmutable());
+                    // call the manager to put it on the database
+                    $manager = $this->getDoctrine()->getManager();
+                    $manager->persist($order);
+                    $manager->flush();
+                    // we put one in database, so the form has been checked
+                    $oneWasChecked = true;
+                }
+            }
+        }
+
+        // if it's checked, stock message and redirect if not the same but the message is error message
+        if ($oneWasChecked) {
+            $this->addFlash('success', "La commande est en bien prise en compte");
+            return $this->render('chest/index.html.twig', [
+                'controller_name' => 'ChestController',
+                'user' => $security->getUser(),
+            ]);
+        } else {
+            $this->addFlash('error', "La commande était vide");
+            return $this->render('chest/index.html.twig', [
+                'controller_name' => 'ChestController',
+                'user' => $security->getUser(),
+            ]);
+        }     
     }
 }
