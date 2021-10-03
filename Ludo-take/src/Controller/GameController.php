50aeb35fd4d7386bbb743174100f7dafd0fed00d
Romain Bourel <romainbourel@outlook.fr>
2021-10-03 14:18:17 +0200

    start modify home page to have background

--- Ludo-take/src/Controller/GameController.php
+++ Ludo-take/src/Controller/GameController.php
@@ -54,7 +54,6 @@ class GameController extends AbstractController
     {
         return $this->render('game/slug.html.twig', [
             'game' => $gameRepository->findBy(['slug' => $slug])[0],
-
         ]);
     }
     
@@ -83,9 +82,9 @@ class GameController extends AbstractController
     /**
      * Method used to display the whole list by number players
      * 
-     * URL : /jeux/{string}  NOM : indexByPlayers
+     * URL : /jeux/joueurs/{string}  NOM : indexByPlayers
      * 
-     *  @Route("/{string}", name="indexByPlayers")
+     *  @Route("/joueurs/{string}", name="indexByPlayers")
      * 
      */
     public function indexByPlayers(string $string, GameRepository $gameRepository, Request $request, PaginatorInterface $paginatorInterface): Response
